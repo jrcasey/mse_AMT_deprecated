a71c6c33c58c11c5f5d6e6ae3883382cea236dea
John Casey <jrcasey@hawaii.edu>
2020-01-30 23:40:40 -1000

    Edited sh loop

--- Job_loop.sh
+++ Job_loop.sh
@@ -5,8 +5,8 @@ STEP=408
 SLEEP=700 #Just over 11 Minutes (in seconds)
 
 for i in $(seq $START $END $STEP) ; do
-    JSTART = $i
-    JEND = $[ $JSTART + $STEP ] 
+    JSTART=$i
+    JEND=$[ $JSTART + $STEP ] 
     echo "Submitting with ${JSTART} and ${JEND}"
     sbatch --array=${JSTART}-${JEND} -p sched_mit_darwin2 --time=12:00:00 job2.sh
     sleep $SLEEP
