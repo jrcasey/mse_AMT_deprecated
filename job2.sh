4115908a688dd2eefd936e9416c5a12483901599
John Casey <jrcasey@hawaii.edu>
2020-04-02 16:07:10 -1000

    AMT debugging

--- job2.sh
+++ job2.sh
@@ -4,10 +4,10 @@
 #SBATCH -o jrcaseyjarray_%A_%a.out # Standard output
 #SBATCH -e jrcaseyjarray_%A_%a.err # Standard error
 
+cd /home/jrcasey/mse_AMT/
+
 module load mit/matlab/2019a
 echo ${SLURM_ARRAY_TASK_ID}
-
-cd /home/jrcasey/mse_AMT/
 
 matlab -nodesktop -nosplash -nojvm < toolbox/wrappers/AMT_Wrapper.m
 sleep 3
