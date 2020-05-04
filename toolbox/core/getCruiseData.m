58cc1d49c77d17ee1abd2f220f4de362b6048349
John Casey <jrcasey@hawaii.edu>
2020-05-03 21:02:40 -1000

    AMT debugging

--- toolbox/core/getCruiseData.m
+++ toolbox/core/getCruiseData.m
@@ -47,7 +47,7 @@ end
 CruiseDat.NitratePlusNitrite = CruiseDat.NitratePlusNitrite .* 1000;
 CruiseDat.Nitrite = CruiseDat.Nitrite .* 1000;
 CruiseDat.Orthophosphate = CruiseDat.Orthophosphate .* 1000;
-CruiseDat.Ammonium = 5 + (CruiseDat.Ammonium .* 1000);
+CruiseDat.Ammonium = 7 + (CruiseDat.Ammonium .* 1000);
 CruiseDat.Silicate = CruiseDat.Silicate .* 1000;
 % Compute nitrate
 CruiseDat.Nitrate = CruiseDat.NitratePlusNitrite - CruiseDat.Nitrite;
